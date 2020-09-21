c1b1e402519e60168780d7f0a9cd9c9e4a7b3827
Taylor Cohron <untraceablez@users.noreply.github.com>
2020-09-21 14:31:49 -0500

    Update discordRichPresencePlex.py
    Added an auto-start function for Windows users. Assuming shortcut in the user startup folder it adds itself as a registry item under HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run

--- discordRichPresencePlex.py
+++ discordRichPresencePlex.py
@@ -10,6 +10,47 @@ import sys
 import tempfile
 import threading
 import time
+import winreg as reg  
+import os              
+  
+#Code to add script to auto-start on Windows startup. THIS METHOD DOES NOT WORK FOR MACOS OR LINUX
+#This code edits the registry, make sure to back up your PC prior to using the auto-start function of this script.
+
+#Before using the auto-start function you'll need to put a shortcut to the plex_discord_rpc.py file 
+#in C:\Users\yourusername\AppData\Microsoft\Windows\Start Menu\Programs\Startup\ the actual file can be in any location on your PC.
+	
+def AddToRegistry(): 
+  
+    # in python __file__ is the instant of 
+    # file path where it was executed  
+    # so if it was executed from desktop, 
+    # then __file__ will be  
+    # c:\users\current_user\desktop 
+    pth = os.path.dirname(os.path.realpath(__file__)) 
+      
+    # name of the python file with extension 
+    s_name="mYscript.py"     
+      
+    # joins the file name to end of path address 
+    address=os.join(pth,s_name)  
+      
+    # key we want to change is HKEY_CURRENT_USER  
+    # key value is Software\Microsoft\Windows\CurrentVersion\Run 
+    key = HKEY_CURRENT_USER 
+    key_value = "Software\Microsoft\Windows\CurrentVersion\Run"
+      
+    # open the key to make changes to 
+    open = reg.OpenKey(key,key_value,0,reg.KEY_ALL_ACCESS) 
+      
+    # modifiy the opened key 
+    reg.SetValueEx(open,"any_name",0,reg.REG_SZ,address) 
+      
+    # now close the opened key 
+    reg.CloseKey(open) 
+  
+# Driver Code 
+if __name__=="__main__": 
+    AddToRegistry() 
 
 class plexConfig:
 
