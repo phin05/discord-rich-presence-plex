d6fd10754d0312b19287877b2afbdae46057dfad
qrenz <renzq@rbhs208.net>
2023-04-18 22:33:39 -0500

    Update constants.py

--- store/constants.py
+++ store/constants.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-name = "Discord Rich Presence for Plex"
+name = "Discord Rich Presence for Plexamp"
 version = "2.3.2"
 
-plexClientID = "discord-rich-presence-plex"
-discordClientID = "413407336082833418"
+plexClientID = "discord-rich-presence-plexamp"
+discordClientID = "1098083071116447815"
 configFilePath = "config.json"
 cacheFilePath = "cache.json"
 logFilePath = "console.log"
