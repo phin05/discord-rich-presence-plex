595e76940c7a33c92c61fa2e4cc5bce9de613375
Phin <59180111+phin05@users.noreply.github.com>
2025-07-08 04:06:00 +0530

    Release v2.14.0

--- app/constants.py
+++ app/constants.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 name = "Discord Rich Presence for Plex"
-version = "2.13.0"
+version = "2.14.0"
 
 plexClientID = "discord-rich-presence-plex"
 discordClientID = "413407336082833418"
